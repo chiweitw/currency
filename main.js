440534c188c99cfb21d9af0f0b685a188d62a00b
Chi-Wei Huang <cwhuang0523@gmail.com>
2018-09-25 01:09:00 +0800

    chanhe wall style for filter effects

--- main.js
+++ main.js
@@ -1,74 +1,99 @@
-var DAMPING = 0.2,
-    GRAVITY = 0.5,
-    MOUSE_SIZE = 100,
-    SPEED = 0.2;
-
+var COEFF = 6,
+    DAMPING = 0.6,
+    GRAVITY = 0.6,
+    SPEED = 0.3,
+    FILTER = 5, //the corrsponded USD for 1 Target currency
+    CANVASWIDTH = 400,
+    CANVASHEIGHT = 400,
+    theta = Math.atan((CANVASWIDTH - FILTER)/CANVASHEIGHT);
 
 var canvas, ctx, TWO_PI = Math.PI * 2, balls = [];
 
-window.requestAnimFrame =
-    window.requestAnimationFrame;
-
-
+window.requestAnimFrame = window.requestAnimationFrame;
 
 window.onload = function () {
     canvas = document.getElementById('myCanvas');
     ctx = canvas.getContext('2d');  
-    canvas.width = 400;
-    canvas.height = 800;
+    canvas.width = CANVASWIDTH;
+    canvas.height = CANVASHEIGHT;
     getAllCurrency();
     update();
 }
-  
-var flagShow = document.getElementById("flagShow");
-flagShow.addEventListener("click", () => {
-    flag();
-});
 
 
-function flag() {
-    console.log("HEY")
-    output = '';
-    console.log(balls.length);
-    for (i=0; i<balls.length; i++) {
-        output += `<a href="#" id="${balls[i].id}"><img src="https://s.xe.com/themes/xe/images/flags/big/${balls[i].id.slice(3, 6).toLowerCase()}.png" alt="${balls[i].id}"></a>`
-    }   
-    document.querySelector(".flags").innerHTML = output;
-}
-
-// Data Loading and produce balls
+///Gata Data drom API///
 function getAllCurrency() {
     var request = new XMLHttpRequest();
     request.open('GET','http://www.apilayer.net/api/live?access_key=fb1ecebbd1b7da06cb66ade116d20658&format=1');
     request.onload = function() {
         var data = JSON.parse(this.response).quotes;
-        ['USDXAU', 'USDBTC'].forEach(e => delete data[e]);
+        ['USDXAU', 'USDBTC','USDCLF','USDXAG'].forEach(e => delete data[e]);
         var country = Object.keys(data);
         var currencyUSDUarget = Object.values(data);
         var currencyTargetUSD = currencyUSDUarget.map(function(element) {
-            return (22*Math.sqrt(1/element));
+
+            return (COEFF*Math.sqrt(1/element)); 
         });
         for (i=0; i<currencyTargetUSD.length; i++) {
-            add_ball(currencyTargetUSD[i], country[i]);
+            add_ball(currencyTargetUSD[i], country[i].slice(3, 6));
         }
     }
     request.send();
 }
 
-var Ball = function(x, y, radius, id) {
+/// Update Callback///
+var update = function() {
+    var delta = SPEED;
+    var i = balls.length;
+    while ( i-- ) {
+      balls[i].apply_force(delta);
+      balls[i].verlet();
+    }  
+    resolve_collisions();
+    check_walls();
+    showResults();
+    beautify();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var i = balls.length;
+    while (i--) balls[i].draw(ctx); 
+    requestAnimFrame(update);
+}
 
+///plot results///
+
+function showResults() {
+    var canvas = document.getElementById('data');
+    var ctx = canvas.getContext('2d'); 
+    canvas.width = CANVASWIDTH + 200;
+    canvas.height = CANVASHEIGHT + 200;
+    var count = 0;
+    for (i=0; i<balls.length; i++) {
+        
+        if (balls[i].radius > FILTER) {
+            var text = balls[i].id.toUpperCase() + ": " + balls[i].radius.toFixed(3);
+            ctx.beginPath();
+            ctx.moveTo(balls[i].x, balls[i].y);
+            ctx.lineTo(CANVASWIDTH+30, (count+1)*20);
+            ctx.strokeStyle="rgba(0,0,0,0.4)";
+            ctx.stroke();
+            ctx.font = "14px Helvetica";
+            ctx.fillText(text, CANVASWIDTH+30, (count+1)*20);
+            count++; 
+        }
+    } 
+}
+
+
+///Ball Object///
+var Ball = function(x, y, radius, id) {
     this.x = x;
     this.y = y;
-
     this.px = x;
     this.py = y;
-
     this.fx = 0;
     this.fy = 0;
-
     this.radius = radius;
-
-    this.id = id;
+    this.id = id.toLowerCase();
 };
 
 var add_ball = function (r, id) {
@@ -81,6 +106,20 @@ var add_ball = function (r, id) {
   
       if (s) balls.push(new Ball(x, y, r, id));
 }
+
+Ball.prototype.draw = function(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, TWO_PI);
+    ctx.shadowBlur=20;
+    ctx.shadowColor="yellow";
+    ctx.fillStyle = 'rgba(255,255,255,1)';
+    ctx.fill();
+    ctx.font = "12px helvetica";
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(0,0,0, 0.8)';
+    var text = this.id;
+    ctx.fillText(text, this.x-ctx.measureText(text).width/2, this.y);
+};
 
 Ball.prototype.apply_force = function(delta) {
 
@@ -104,19 +143,6 @@ Ball.prototype.verlet = function() {
     this.y = ny;
 };
 
-Ball.prototype.draw = function(ctx) {
-
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, TWO_PI);
-    ctx.fillStyle = 'rgba(255,255,255,0.7)';
-    ctx.fill();
-    ctx.font = "8px helvetica";
-    ctx.beginPath();
-    ctx.fillStyle = 'rgba(0,0,0,0.7)';
-    ctx.fillText(this.id, this.x, this.y);
-};
-
-
 var resolve_collisions = function(ip) {
     var i = balls.length;
     while (i--) {
@@ -131,10 +157,6 @@ var resolve_collisions = function(ip) {
             var dist      = Math.sqrt(length);
             var real_dist = dist - (ball_1.radius + ball_2.radius);
             if (real_dist < 0) {
-                var vel_x1 = ball_1.x - ball_1.px;
-                var vel_y1 = ball_1.y - ball_1.py;
-                var vel_x2 = ball_2.x - ball_2.px;
-                var vel_y2 = ball_2.y - ball_2.py;
                 var depth_x = diff_x * (real_dist / dist);
                 var depth_y = diff_y * (real_dist / dist);
                 ball_1.x -= depth_x * 0.5;
@@ -148,8 +170,60 @@ var resolve_collisions = function(ip) {
 
 var check_walls = function() {
     var i = balls.length;
+
     while (i--) {
         var ball = balls[i];
+        var d = ball.x*Math.cos(theta)-ball.y*Math.sin(theta)-ball.radius;
+        var d2 = (CANVASWIDTH-ball.x)*Math.cos(theta)-ball.y*Math.sin(theta)-ball.radius;
+        var d3 = ball.x*Math.cos(theta)-(CANVASHEIGHT-ball.y)*Math.sin(theta)-ball.radius;
+        var d4 = (CANVASWIDTH-ball.x)*Math.cos(theta)-(CANVASHEIGHT-ball.y)*Math.sin(theta)-ball.radius;
+        var hc = 0.5*CANVASHEIGHT - (ball.radius - 0.5*FILTER*Math.cos(theta))/Math.sin(theta);
+
+        if (ball.y < CANVASHEIGHT*0.5){
+            if (d<=0) {
+                var vel_x = ball.px - ball.x;
+                ball.x -= d*Math.cos(theta);
+                ball.px = ball.x - vel_x * DAMPING;
+                var vel_y = ball.py - ball.y;
+                ball.y -= d*Math.sin(theta);
+                ball.py = ball.y - vel_y * DAMPING;
+            }
+            if (d2<=0) {
+                var vel_x = ball.px - ball.x;
+                ball.x += d2*Math.cos(theta);
+                ball.px = ball.x - vel_x * DAMPING;
+                var vel_y = ball.py - ball.y;
+                ball.y -= d2*Math.sin(theta);
+                ball.py = ball.y - vel_y * DAMPING;
+            }
+        } else {
+            if (d3<=0) {
+                var vel_x = ball.px - ball.x;
+                ball.x -= d3*Math.cos(theta);
+                ball.px = ball.x - vel_x * DAMPING;
+                var vel_y = ball.py - ball.y;
+                ball.y += d3*Math.sin(theta);
+                ball.py = ball.y - vel_y * DAMPING;
+            }
+            if (d4<=0) {
+                var vel_x = ball.px - ball.x;
+                ball.x += d4*Math.cos(theta);
+                ball.px = ball.x - vel_x * DAMPING;
+                var vel_y = ball.py - ball.y;
+                ball.y += d4*Math.sin(theta);
+                ball.py = ball.y - vel_y * DAMPING;
+            }
+        }
+
+       
+        while (ball.y > hc && ball.radius > FILTER) {
+                var diff = ball.y - hc;
+                var vel_y = ball.py - ball.y;
+                ball.y -= diff ; 
+                ball.py = ball.y - vel_y * DAMPING;
+        }
+
+
         if (ball.x < ball.radius) {
             var vel_x = ball.px - ball.x;
             ball.x = ball.radius;
@@ -167,25 +241,30 @@ var check_walls = function() {
             var vel_y = ball.py - ball.y;
             ball.y = canvas.height - ball.radius;
             ball.py = ball.y - vel_y * DAMPING;
-        }
+        }      
     }
 }
 
-var update = function() {
-    var delta = SPEED;
-    var i = balls.length;
-    while ( i-- ) {
-      balls[i].apply_force(delta);
-      balls[i].verlet();
-    }
-  
-    resolve_collisions();
-    check_walls();
-    
-  
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var i = balls.length;
-    while (i--) balls[i].draw(ctx);
-  
-    requestAnimFrame(update);
+
+///Derecorating///
+function beautify() {
+    var canvas = document.getElementById('data');
+    var ctx = canvas.getContext('2d'); 
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(CANVASWIDTH*0.5-FILTER*0.5, CANVASHEIGHT*0.5);
+    ctx.lineTo(0, CANVASHEIGHT);
+
+
+    ctx.moveTo(CANVASWIDTH,0);
+    ctx.lineTo(CANVASWIDTH*0.5+FILTER*0.5, CANVASHEIGHT*0.5);
+    ctx.lineTo(CANVASWIDTH, CANVASHEIGHT);
+    ctx.strokeStyle="transparent";
+    ctx.stroke();
+
+    ctx.fillStyle = "rgba(0,0,0,0.3)";
+    ctx.fill();
 }
+
+
+
